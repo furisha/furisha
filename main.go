0fe55c04cf5a590c95ea7e6dda34876de7525c73
Vladan Furman <vladanfurman@gmail.com>
2018-09-30 12:44:13 +0200

    you're doing great

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	)
 
-func main()  {
-	fmt.Print("wassssup")
+func main() {
+	fmt.Println("Hello everyone, im the best go programmer in the world")
+
+	foo()
+
+	fmt.Println("sdssdsgs")
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+
+	bar()
 }
+
+func foo() {
+	fmt.Println("I'm in foo")
+}
+
+func bar()  {
+	fmt.Println("Ludnica")
+}
+
+// control flow:
+// (1) sequence
+// (2) loop; iterative
+// (3) conditional
